7ef7a41f105e8855e8a39aa199812d50dd58a4ee
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-09-27 20:01:08 -0700

    Reformat Form.js

--- src/Form.js
+++ src/Form.js
@@ -3,51 +3,57 @@ import useForm from "./useForm";
 import validate from './LoginValidationRules';
 
 const Form = () => {
-    const login = () => {
-      console.log(values);
-    }
+  const login = () => {
+    console.log(values);
+  }
 
-    const { values, handleChange, handleSubmit, errors } = useForm(login, validate);
+  const { values, handleChange, handleSubmit, errors } = useForm(login, validate);
 
-	return (
-		<div className="section is-fullheight">
-			<div className="container">
-				<div className="column is-4 is-offset-4">
-					<div className="box">
-						<form onSubmit={handleSubmit}>
-							<div className="field">
-								<label className="label">Email Address</label>
-								<div className="control">
-									<input
-                                    className={`input ${errors.email && 'is-danger'}`}
-                                    type="email"
-                                    name="email"
-                                    onChange={handleChange}
-                                    value={values.email || ''}
-                                    required
-                                    />
-								</div>
-							</div>
-							<div className="field">
-								<label className="label">Password</label>
-								<div className="control">
-									<input
-                                    className={`input ${errors.password && 'is-danger'}`}
-                                    type="password"
-                                    name="password"
-                                    onChange={handleChange}
-                                    value={values.password || ''}
-                                    required
-                                    />
-								</div>
-							</div>
-							<button type="submit" className="button is-block is-info is-fullwidth">Login</button>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="section is-fullheight">
+      <div className="container">
+        <div className="column is-4 is-offset-4">
+          <div className="box">
+            <form onSubmit={handleSubmit}>
+              <div className="field">
+                <label className="label">Email Address</label>
+                <div className="control">
+                  <input
+                    className={`input ${errors.email && 'is-danger'}`}
+                    type="email"
+                    name="email"
+                    onChange={handleChange}
+                    value={values.email || ''}
+                    required
+                  />
+                  { errors.email && (
+                    <p className="help is-danger">{errors.email}</p>
+                  )}
+                </div>
+              </div>
+              <div className="field">
+                <label className="label">Password</label>
+                <div className="control">
+                  <input
+                    className={`input ${errors.password && 'is-danger'}`}
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                    value={values.password || ''}
+                    required
+                  />
+                  { errors.password && (
+                    <p className="help is-danger">{errors.password}</p>
+                  )}
+                </div>
+              </div>
+              <button type="submit" className="button is-block is-info is-fullwidth">Login</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Form;
